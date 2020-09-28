fe7d3d5bac030bbcaa89ac14ce9e0a90cce3c8a9
Farhan <fsyed7x@gmail.com>
2020-09-28 23:12:54 +0530

    asd

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { ExampleComponent } from 'image_compress_crop_preview'
+import { Squared } from 'image_compress_crop_preview'
 import 'image_compress_crop_preview/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [Output, setOutput] = useState()
+  
+  useEffect(() => {
+    console.log(Output)
+  }, [Output])
+
+  return (
+    <div>
+      <Squared defaultImg="" color="" size="" setOutput={setOutput}/>
+    </div>
+  )
 }
+// defaultImage="https://cactusthemes.com/blog/wp-content/uploads/2018/01/tt_avatar_small.jpg"
 
 export default App
